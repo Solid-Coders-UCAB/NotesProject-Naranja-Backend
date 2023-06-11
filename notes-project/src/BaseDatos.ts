94fc3b43834bdb7c1476a6f7821ea832f1ecc81f
Cesar Garcia <cesalgargil@gmail.com>
2023-06-11 18:52:48 -0400

    Migracion ahora funciona
    Se agrego "autoLoadEntities: true" para que las entidades pudieran ubicarse para realizar la migracion

--- notes-project/src/BaseDatos.ts
+++ notes-project/src/BaseDatos.ts
@@ -4,14 +4,14 @@ require('dotenv').config();
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
 
-            type: 'postgres',
-            host: "localhost",
-            port: 5432,
-            database: "NoteProject",
-            username:"postgres",
-            password:"omar25",
-            synchronize: true,
-            logging: true
-       
-
+    type: 'postgres',
+    host: "localhost",
+    port: 5432,
+    database: "NoteApp",
+    username:"postgres",
+    password:"Cesar24Diana14",
+    autoLoadEntities: true,
+    synchronize: true,
+    logging: true
+    
 }
