b4fa3d578d2a174073179753ec158928568a017c
OmarJMM <omarmelendez250101@gmail.com>
2023-06-11 19:31:32 -0400

    Agregadoarchivo .env para la conexion a la bd

--- notes-project/src/BaseDatos.ts
+++ notes-project/src/BaseDatos.ts
@@ -5,11 +5,11 @@ require('dotenv').config();
 export const typeOrmConfig: TypeOrmModuleOptions = {
 
     type: 'postgres',
-    host: "localhost",
-    port: 5432,
-    database: "NoteApp",
-    username:"postgres",
-    password:"Cesar24Diana14",
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT),
+    database: process.env.DB_NAME,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
     autoLoadEntities: true,
     synchronize: true,
     logging: true
